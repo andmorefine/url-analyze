6f508cae9ef672612310d19498729baf1421ddec
tanaka.satoshi <tanaka.satoshi@a-tm.co.jp>
2024-03-31 19:25:47 +0900

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,15 @@
+// @see: https://github.com/vercel/next.js/issues/43704
+// /middleware.ts
 import { NextResponse } from 'next/server'
 
 export function middleware(request: Request) {
+  // Store current request url in a custom header, which you can read later
   const requestHeaders = new Headers(request.headers)
   requestHeaders.set('x-url', request.url)
 
   return NextResponse.next({
     request: {
+      // Apply new request headers
       headers: requestHeaders,
     },
   })
